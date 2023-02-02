b3b4428fbf9d9e5bb2bf7aedbd4cabb37f749828
Shrmanto <shrmantoch@gmail.com>
2023-02-02 14:47:28 +0700

    up

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+from tkinter import messagebox, filedialog
+import pandas as pd, json, os, customtkinter as tk
+
+root = Tk()
+root.geometry("400x300")
+
+root.mainloop()
